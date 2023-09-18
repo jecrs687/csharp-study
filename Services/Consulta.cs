3b090eb79f3a38c26fb6c157c282e6d5b8311e70
Emanuel Cascone <emanuelcascone@gmail.com>
2023-09-18 16:23:04 -0300

    fix: format

--- Services/Consulta.cs
+++ Services/Consulta.cs
@@ -5,10 +5,10 @@ namespace dot_net_study.Services
 {
 	public class Consulta
 	{
-		private Dictionary<int, string> users = new ()
+		private Dictionary<int, string> users = new()
 		{
 			{1, "Giuseppin"},
-            {2, "André"}
+			{2, "André"}
 		};
 		public Consulta() { }
 		public void View()
@@ -16,22 +16,22 @@ namespace dot_net_study.Services
 			Console.WriteLine("Escreva o id do usuário que deseja pesquisar: ");
 			try
 			{
-                int userId = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine(Search(userId));
-            }
-            catch (KeyNotFoundException ex)
-            {
+				int userId = Convert.ToInt32(Console.ReadLine());
+				Console.WriteLine(Search(userId));
+			}
+			catch (KeyNotFoundException ex)
+			{
 				Console.WriteLine("Usuário não encontrado, error: ", ex.Message);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Erro ao buscar o usuário", ex.Message);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Erro ao buscar o usuário", ex.Message);
 				throw new Exception("Test");
-            }
-        }
+			}
+		}
 		public string Search(int a)
 		{
-            return users[a];
+			return users[a];
 		}
 
 	}
